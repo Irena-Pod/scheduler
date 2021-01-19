bf16062e68b726bbe0204968a5ca38a995da9402
Irena-Pod <irena.podgurski@gmail.com>
2021-01-19 13:46:06 -0500

    add InterviewerList component

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss"
+
+export default function InterviewerList(props) {
+  const { interviewers } = props;
+
+  let parsedInterviewersList = [];
+
+  if (Array.isArray(interviewers)) {
+    parsedInterviewersList = interviewers.map((interviewer) => {
+      return (
+        <InterviewerListItem
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === props.interviewer}
+          setInterviewer={props.setInterviewer}
+        />
+      );
+    });
+  }
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list" onClick={() => props.setInterviewer}>{parsedInterviewersList}</ul>
+    </section>
+  );
+}
