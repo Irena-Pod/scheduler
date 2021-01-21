d1847d614dc0a09ba4de33c21837a571c489e656
Irena-Pod <irena.podgurski@gmail.com>
2021-01-21 13:18:33 -0500

    update application to retrieve appointments from API

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -5,72 +5,33 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import "components/Appointment ";
 import Appointment from "components//Appointment /index";
+import { getAppointmentsForDay } from "../helpers/selectors"
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Irena Podgurski",
-      interviewer: {
-        id: 1,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      },
-    },
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Adam Ali",
-      interviewer: {
-        id: 1,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      },
-    },
-  },
-];
 
 export default function Application(props) {
   const [state, setState] = useState({
     days: [],
     day: "Monday",
+    appointments: {}
   });
 
+
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => {
-    setState((prev) => ({ ...prev, days }));
-  };
 
   useEffect(() => {
-    axios.get("/api/days").then((res) => {
-      setDays([...res.data]);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ])
+    .then((all) => {
+    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    console.log(all[1].data)
     });
   }, []);
 
-  const parsedAppointments = appointments.map((appointment) => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const appointment = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
@@ -93,7 +54,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {parsedAppointments}
+        {appointment}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
